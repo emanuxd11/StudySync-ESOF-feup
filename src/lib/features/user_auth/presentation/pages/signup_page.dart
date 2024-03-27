2384f378c1d9807f6b08b58070f5ac427eef59aa
Emanuel Maia <tavanomanu@gmail.com>
2024-03-27 00:51:33 +0000

    Add firebase registration.

--- src/lib/features/user_auth/presentation/pages/signup_page.dart
+++ src/lib/features/user_auth/presentation/pages/signup_page.dart
@@ -1,11 +1,34 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
+import 'package:flutter/widgets.dart';
+import 'package:study_sync/features/user_auth/firebase_auth_impl/firebase_auth_services.dart';
 import 'package:study_sync/features/user_auth/presentation/pages/login_page.dart';
 import 'package:study_sync/features/user_auth/presentation/widget/form_container_widget.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatefulWidget {
   const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+
+  final FirebaseAuthService _auth = FirebaseAuthService();
+
+  TextEditingController _usernameController = TextEditingController();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,43 +52,49 @@ class SignUpPage extends StatelessWidget {
                     const SizedBox(
                       height: 30,
                     ),
-                    const FormContainerWidget(
+                    FormContainerWidget(
+                      controller: _usernameController,
                       hintText: "Username",
                       isPasswordField: false,
                     ),
                     const SizedBox(
                       height: 10,
                     ),
-                    const FormContainerWidget(
+                    FormContainerWidget(
+                      controller: _emailController,
                       hintText: "Email",
                       isPasswordField: false,
                     ),
                     const SizedBox(
                       height: 10,
                     ),
-                    const FormContainerWidget(
+                    FormContainerWidget(
+                      controller: _passwordController,
                       hintText: "Password",
                       isPasswordField: true,
                     ),
                     const SizedBox(
                       height: 30,
                     ),
-                    Container(
-                      width: double.infinity,
-                      height: 45,
-                      decoration: BoxDecoration(
-                        color: Colors.green,
-                        borderRadius: BorderRadius.circular(10),
+                    GestureDetector(
+                      onTap: _signUp,
+                      child: Container(
+                        width: double.infinity,
+                        height: 45,
+                        decoration: BoxDecoration(
+                          color: Colors.green,
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: const Center(
+                            child: Text("Sign Up",
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ))),
                       ),
-                      child: const Center(
-                          child: Text("Sign Up",
-                              style: TextStyle(
-                                fontSize: 20,
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ))),
                     ),
-                    SizedBox(height: 20, ),
+                    SizedBox(height: 20,),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -94,5 +123,20 @@ class SignUpPage extends StatelessWidget {
                     )
                   ],
                 ))));
+  }
+
+  void _signUp() async {
+    String username = _usernameController.text;
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    User? _user = await _auth.signUpWithEmailAndPassword(email, password);
+
+    if (User != null) {
+      print("User created successfully!");
+      Navigator.pushNamed(context, "/");
+    } else {
+      print("Error in registering user");
+    }
   }
 }
