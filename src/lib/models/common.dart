6278ad0bd95157b21b5d4c416b125000e278d6ff
Emanuel Maia <tavanomanu@gmail.com>
2024-05-03 11:48:10 +0100

    Update bottom bar with route to create session widget.

--- src/lib/models/common.dart
+++ src/lib/models/common.dart
@@ -28,15 +28,15 @@ class CommonScreen extends StatelessWidget {
             backgroundColor: Colors.green,
           ),
           BottomNavigationBarItem(
+            icon: Icon(Icons.playlist_add),
+            label: 'Create Session',
+            backgroundColor: Colors.green,
+          ),
+          BottomNavigationBarItem(
             icon: Icon(Icons.list_alt),
             label: 'Exams',
             backgroundColor: Colors.green,
           ),
-          /* BottomNavigationBarItem(
-            icon: Icon(Icons.playlist_add),
-            label: 'Sessions',
-            backgroundColor: Colors.green,
-          ), */
           BottomNavigationBarItem(
             icon: Icon(Icons.settings),
             label: 'Settings',
@@ -48,9 +48,14 @@ class CommonScreen extends StatelessWidget {
             case 0:
               context.go(HomePage.routeName);
             case 1:
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CreateSession()
+                )
+              );
+            case 2:
               context.go(ExamsScreen.fullPath);
-            /* case 2:
-              context.go(SessionsScreen.fullPath); */
             case 2: //3
               context.go(SettingsScreen.fullPath);
           }
