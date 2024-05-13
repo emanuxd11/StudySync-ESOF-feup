b70f6379bc912f6705da2e9c490e2cad3f74dd7f
Emanuel Maia <tavanomanu@gmail.com>
2024-05-13 23:08:36 +0100

    Sort sessions by date.

--- src/lib/screens/home.dart
+++ src/lib/screens/home.dart
@@ -153,8 +153,11 @@ class _HomePageState extends State<HomePage> {
           Expanded(
             child: selections[1]
                 ? StreamBuilder<QuerySnapshot>(
-                stream: FirebaseFirestore.instance.collection('sessions').snapshots(),
-                builder: (context, snapshots) {
+              stream: FirebaseFirestore.instance
+                  .collection('sessions')
+                  .orderBy('time', descending: false) // Sort sessions by time (ascending)
+                  .snapshots(),
+              builder: (context, snapshots) {
                 if (snapshots.connectionState == ConnectionState.waiting || snapshots.data == null) {
                   return const Center(
                     child: CircularProgressIndicator(),
@@ -170,93 +173,94 @@ class _HomePageState extends State<HomePage> {
                 return ListView.builder(
                   itemCount: filteredData.length,
                   itemBuilder: (context, index) {
-                  var data = filteredData[index];
+                    var data = filteredData[index];
 
-                  bool isMember = false;
-                  try {
-                    for (var member in data['members']) {
-                      if (member == FirebaseAuth.instance.currentUser?.uid) {
-                        isMember = true;
-                      }
-                    }
-                  } catch (e) { /* don't do anything lol */ }
-
-                  if (isMember) {
-                    return const SizedBox.shrink();
-                  }
-
-                  return ListTile(
-                    title: Text(
-                      data['courseName'],
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
-                      style: const TextStyle(
-                        color: Colors.black,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    subtitle: Padding(
-                      padding: const EdgeInsets.only(left: 10.0),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(data['topic'], style: const TextStyle(fontSize: 12.0),),
-                          Text(data['place'], style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold)),
-                          Text(data['time'].toString(), style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold))
-                        ],
-                      ),
-                    ),
-                    trailing: ElevatedButton(
-                      onPressed: () {
-                        // Join session logic
-                        String sessionId = data['id'];
-                        DocumentReference ref =
-                        FirebaseFirestore.instance.collection('sessions').doc(sessionId);
-                        FirebaseAuth auth = FirebaseAuth.instance;
-                        String userId = '';
-                        if (auth.currentUser != null) {
-                          userId = auth.currentUser!.uid;
+                    bool isMember = false;
+                    try {
+                      for (var member in data['members']) {
+                        if (member == FirebaseAuth.instance.currentUser?.uid) {
+                          isMember = true;
                         }
+                      }
+                    } catch (e) { /* don't do anything lol */ }
 
-                        ref.update({
-                          'members': FieldValue.arrayUnion([userId])
-                        }).then((_) {
-                          print('User $userId added to session $sessionId');
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            SnackBar(
-                              content: Text("You are now a member of ${data['topic']}!"),
-                            ),
-                          );
-                        }).catchError((error) {
-                          print('Failed to add user to session: $error');
-                        });
+                    if (isMember) {
+                      return const SizedBox.shrink();
+                    }
 
-                        // Navigate to the chat screen
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ChatScreen(
-                              sessionId: data['id'],
-                              sessionTopic: data['topic'],
-                            ),
-                          ),
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.green,
-                        textStyle: const TextStyle(
-                          fontSize: 14,
-                          fontWeight: FontWeight.bold,
+                    return ListTile(
+                      title: Text(
+                        data['topic'],
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold
                         ),
                       ),
-                      child: const Text('Join'),
-                    ),
-                  );
-                  });
-                },
-              )
-            : StreamBuilder<QuerySnapshot>(
+                      subtitle: Padding(
+                        padding: const EdgeInsets.only(left: 10.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(data['courseName'], style: const TextStyle(fontSize: 12.0),),
+                            Text(data['place'], style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold)),
+                            Text(data['time'].toString(), style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold))
+                          ],
+                        ),
+                      ),
+                      trailing: ElevatedButton(
+                        onPressed: () {
+                          // Join session logic
+                          String sessionId = data['id'];
+                          DocumentReference ref =
+                          FirebaseFirestore.instance.collection('sessions').doc(sessionId);
+                          FirebaseAuth auth = FirebaseAuth.instance;
+                          String userId = '';
+                          if (auth.currentUser != null) {
+                            userId = auth.currentUser!.uid;
+                          }
+
+                          ref.update({
+                            'members': FieldValue.arrayUnion([userId])
+                          }).then((_) {
+                            print('User $userId added to session $sessionId');
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              SnackBar(
+                                content: Text("You are now a member of ${data['topic']}!"),
+                              ),
+                            );
+                          }).catchError((error) {
+                            print('Failed to add user to session: $error');
+                          });
+
+                          // Navigate to the chat screen
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => ChatScreen(
+                                sessionId: data['id'],
+                                sessionTopic: data['topic'],
+                              ),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.green,
+                          textStyle: const TextStyle(
+                            fontSize: 14,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        child: const Text('Join'),
+                      ),
+                    );
+                  },
+                );
+              },
+            )
+                : StreamBuilder<QuerySnapshot>(
               stream: FirebaseFirestore.instance.collection('sessions').where('members', arrayContains: FirebaseAuth.instance.currentUser!.uid).snapshots(),
               builder: (context, snapshots) {
                 if (snapshots.connectionState == ConnectionState.waiting || snapshots.data == null) {
@@ -266,11 +270,11 @@ class _HomePageState extends State<HomePage> {
                 }
 
                 var filteredData = snapshots.data!.docs.where((doc) {
-                var courseName = doc['courseName'].toString().toLowerCase();
-                var topic = doc['topic'].toString().toLowerCase();
-                var place = doc['place'].toString().toLowerCase();
-                var time = doc['time'].toString().toLowerCase();
-                return courseName.contains(search.toLowerCase()) || topic.contains(search.toLowerCase()) || place.contains(search.toLowerCase()) || time.contains(search.toLowerCase());
+                  var courseName = doc['courseName'].toString().toLowerCase();
+                  var topic = doc['topic'].toString().toLowerCase();
+                  var place = doc['place'].toString().toLowerCase();
+                  var time = doc['time'].toString().toLowerCase();
+                  return courseName.contains(search.toLowerCase()) || topic.contains(search.toLowerCase()) || place.contains(search.toLowerCase()) || time.contains(search.toLowerCase());
                 }).toList();
                 return ListView.builder(
                     itemCount: filteredData.length,
@@ -279,7 +283,7 @@ class _HomePageState extends State<HomePage> {
 
                       return ListTile(
                         title: Text(
-                          data['courseName'],
+                          data['topic'],
                           maxLines: 1,
                           overflow: TextOverflow.ellipsis,
                           style: const TextStyle(
@@ -293,7 +297,7 @@ class _HomePageState extends State<HomePage> {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              Text(data['topic'], style: const TextStyle(fontSize: 12.0),),
+                              Text(data['courseName'], style: const TextStyle(fontSize: 12.0),),
                               Text(data['place'], style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold)),
                               Text(data['time'].toString(), style: const TextStyle(fontSize: 13.0, fontWeight: FontWeight.bold))
                             ],
@@ -351,6 +355,7 @@ class _HomePageState extends State<HomePage> {
               },
             ),
           ),
+
         ]
       ),
     );
